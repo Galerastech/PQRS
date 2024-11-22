a4249b2d3f051c22303baa9caa6554506ac259ae
bayronrc <ryshen98@outlook.com>
2024-11-22 17:10:33 -0500

    add:basic-fields-tenants-and-users-table

--- backend/models.py
+++ backend/models.py
@@ -1,15 +1,36 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
-
+from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey
+from sqlalchemy.orm import relationship
 from backend.database import Base
+
+
+class Tenant(Base):
+    __tablename__ = 'tenants'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    email = Column(String(150), nullable=False, unique=True)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
+    users = relationship("Users", back_populates="tenants")
 
 
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
+    tenant_id = Column(Integer, ForeignKey('tenants.id'), nullable=False)
     username = Column(String(50), unique=True, nullable=False)
     email = Column(String(50), unique=True, nullable=False)
     password = Column(String(100), nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
+
+    tenat = relationship("Tenant", back_populates="users")
 
 
-
+class Subscription(Base):
+    __tablename__ = 'subscriptions'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tenant_id = Column(Integer, ForeignKey('tenants.id'), nullable=False)
+    start_date = Column(DateTime, default=func.now())
+    end_date = Column(DateTime)
+    status = Column(String(50), nullable=False, default='active')
