4a7fa69370d32dd319b4ab4854cd96de33e85788
bayronrc <ryshen98@outlook.com>
2024-11-25 08:51:18 -0500

    añadiendo-estructura-basica-de-base-de-datos

--- backend/models.py
+++ backend/models.py
@@ -11,26 +11,29 @@ class Tenant(Base):
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
-    users = relationship("Users", back_populates="tenants")
+    users = relationship("User", back_populates="tenant", cascade="all,delete-orphan")
+    subscriptions = relationship("Subscriptions", back_populates="tenant", cascade="all,delete-orphan")
 
 
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    tenant_id = Column(Integer, ForeignKey('tenants.id'), nullable=False)
+    tenant_id = Column(Integer, ForeignKey('tenants.id',ondelete="CASCADE"), nullable=False)
     username = Column(String(50), unique=True, nullable=False)
     email = Column(String(50), unique=True, nullable=False)
     password = Column(String(100), nullable=False)
     created_at = Column(DateTime, server_default=func.now())
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
 
-    tenat = relationship("Tenant", back_populates="users")
+    tenant = relationship("Tenant", back_populates="users")
 
 
 class Subscription(Base):
     __tablename__ = 'subscriptions'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    tenant_id = Column(Integer, ForeignKey('tenants.id'), nullable=False)
+    tenant_id = Column(Integer, ForeignKey('tenants.id', ondelete="CASCADE"), nullable=False)
     start_date = Column(DateTime, default=func.now())
     end_date = Column(DateTime)
     status = Column(String(50), nullable=False, default='active')
+
+    tenant = relationship("Tenant", back_populates="subscriptions")
