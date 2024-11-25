19e07f1115fdf2e313c71f8cc2da0510848f079a
bayronrc <ryshen98@outlook.com>
2024-11-25 14:08:37 -0500

    mejorando la responsividad de la interfaz

--- layouts/auth_layout.py
+++ layouts/auth_layout.py
@@ -8,15 +8,14 @@ class AuthLayout:
         self.image = ft.Image(
             src="images/loginImage.jpg",
             fit=ft.ImageFit.COVER,
-            width=840,
-            height=480,
+            width=860,
+            height=540,
             border_radius=ft.border_radius.only(top_left=12, bottom_left=12),
-            expand=2
         )
         self.page.on_resize = self.on_resize
 
     def on_resize(self, e):
-        self.image.visible = self.page.width >= 600
+        self.image.visible = self.page.width >= 428
         self.page.update()
 
     def update_content(self, new_content):
@@ -27,13 +26,17 @@ class AuthLayout:
         return ft.ResponsiveRow(
             controls=[
                 ft.Card(
+                    col={"lg": 8},
                     color=ft.colors.SECONDARY_CONTAINER,
                     content=ft.Row(
                         controls=[
                             self.image,
                             self.content
                         ],
+                        vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                        alignment=ft.MainAxisAlignment.CENTER
                     ),
+                    width=1200
                 ),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
