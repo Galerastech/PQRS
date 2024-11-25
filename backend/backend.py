9b047a94f32839b97a105e5558db1e850f9814b7
bayronrc <ryshen98@outlook.com>
2024-11-25 18:06:09 -0500

    añadiendo-auth-jwt

--- backend/backend.py
+++ backend/backend.py
@@ -8,24 +8,30 @@ from backend.database import get_db
 from backend.models import User
 from backend.schemas import UserSchema
 from backend.services import UserService
-
-app = FastAPI(
-    title="PQRS",
-    docs_url="/app/docs",
-
-)
-
-user_service = UserService()
+from fastapi import APIRouter
 
 
-@app.post("/auth/signup",tags=["Auth"])
-async def sign_up(user: UserSchema, db: Session = Depends(get_db)):
-    exist_user = db.query(User).filter(
-        (User.email == user.email) |
-        (User.username == user.username)
-    ).first()
+router = APIRouter()
 
-    if exist_user:
-        raise HTTPException(status_code=400, detail="Usuario ya existe en la base de datos")
-    new_user = user_service.create_user(db=db, user=user)
-    return new_user
+router.add_route()
+
+# app = FastAPI(
+#     title="PQRS",
+#     docs_url="/app/docs",
+#
+# )
+#
+# user_service = UserService()
+#
+#
+# @app.post("/auth/signup",tags=["Auth"])
+# async def sign_up(user: UserSchema, db: Session = Depends(get_db)):
+#     exist_user = db.query(User).filter(
+#         (User.email == user.email) |
+#         (User.username == user.username)
+#     ).first()
+#
+#     if exist_user:
+#         raise HTTPException(status_code=400, detail="Usuario ya existe en la base de datos")
+#     new_user = user_service.create_user(db=db, user=user)
+#     return new_user
---
+++ backend/routes/auth.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+
+router = APIRouter()
+
+
+router.add_route(auth_route)
