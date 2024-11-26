d3552973a65aa2566f8701a3e91cf1edb6a9c6bb
bayronrc <ryshen98@outlook.com>
2024-11-26 18:01:47 -0500

    inicializacion de los modelos

--- backend/models.py
+++ backend/models.py
@@ -5,35 +5,43 @@ from backend.database import Base
 
 class Tenant(Base):
     __tablename__ = 'tenants'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(255), nullable=False)
-    email = Column(String(150), nullable=False, unique=True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    nit = Column(String, nullable=False, unique=True)
+    address = Column(String, nullable=True)
+    phone = Column(String, nullable=True)
+    status = Column(String, nullable=False, default='activo')
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
-
-    users = relationship("User", back_populates="tenant", cascade="all,delete-orphan")
-    subscriptions = relationship("Subscriptions", back_populates="tenant", cascade="all,delete-orphan")
-
 
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    tenant_id = Column(Integer, ForeignKey('tenants.id',ondelete="CASCADE"), nullable=False)
-    username = Column(String(50), unique=True, nullable=False)
-    email = Column(String(50), unique=True, nullable=False)
-    password = Column(String(100), nullable=False)
-    created_at = Column(DateTime, server_default=func.now())
-    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    apartment = Column(String, nullable=False)
+    phone = Column(String, nullable=True)
+    status = Column(String, nullable=False, default='activo')
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    tenant_id = Column(Integer, ForeignKey('tenants.id', ondelete='CASCADE'), nullable=False)
 
-    tenant = relationship("Tenant", back_populates="users")
+    tenant = relationship('Tenant', backref='users')
+
 
 
 class Subscription(Base):
     __tablename__ = 'subscriptions'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    tenant_id = Column(Integer, ForeignKey('tenants.id', ondelete="CASCADE"), nullable=False)
-    start_date = Column(DateTime, default=func.now())
-    end_date = Column(DateTime)
-    status = Column(String(50), nullable=False, default='active')
+    id = Column(Integer, primary_key=True)
+    payment_method = Column(String, nullable=False)
+    modality = Column(String, nullable=False)
+    start_date = Column(DateTime, nullable=False, default=func.now())
+    end_date = Column(DateTime, nullable=False)
+    duration = Column(Integer, nullable=False)
+    status = Column(String, nullable=False, default='activo')
+    tenant_id = Column(Integer, ForeignKey('tenants.id', ondelete='CASCADE'), nullable=False)
 
-    tenant = relationship("Tenant", back_populates="subscriptions")
+    tenant = relationship('Tenant', backref='subscriptions')
+
+
